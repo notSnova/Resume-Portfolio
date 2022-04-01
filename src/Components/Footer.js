179ec99bf971b328fc402e5cb0e60dfc27c336b7
Snova <dafanifialfalz@gmail.com>
2022-04-02 05:26:26 +0800

    Danial's Resume

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -22,8 +22,8 @@ const Footer = ({ data }) => {
           <ul className="copyright">
             <li>
               Made by{" "}
-              <a title="PAPA" href="http://www.papareact.com/">
-                PAPA
+              <a title="DANIAL" href="https://github.com/notSnova">
+                DANIAL
               </a>
             </li>
           </ul>
